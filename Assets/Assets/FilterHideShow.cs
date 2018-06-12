4885375e7a3b8686d2c34c2f622d3195bc6e868c
carlos victor quijano <cquijanochavez@gmail.com>
2018-06-12 14:22:07 -0300

    fix dialog filter

---
+++ Assets/Assets/FilterHideShow.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FilterHideShow : MonoBehaviour {
+    public bool isLocked;
+    CanvasGroup filterCanvas;
+    void Start ()
+    {
+        GameObject filterObj = GameObject.FindGameObjectWithTag("FilterCanvas");
+        filterCanvas = filterObj.GetComponent<CanvasGroup>();
+
+    }
+
+    private void Update()
+    {
+        if(Input.GetKeyUp(KeyCode.F))
+        {
+            if (isLocked) Show(); else Hide();
+            isLocked = !isLocked;
+        }
+    }
+
+    void Hide()
+    {
+        filterCanvas.alpha = 0f; //this makes everything transparent
+        filterCanvas.blocksRaycasts = false; //this prevents the UI element to receive input events
+    }
+
+    void Show()
+    {
+        filterCanvas.alpha = 1f;
+        filterCanvas.blocksRaycasts = true;
+    }
+}
