60b724278c42c9cd3877ac19307c3ea64a366d2f
Aimee <aimeesc@gmail.com>
2018-06-12 17:19:12 -0300

    aime

--- Assets/Assets/MouseController.cs
+++ Assets/Assets/MouseController.cs
@@ -2,60 +2,74 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class MouseController : MonoBehaviour {
+public class MouseController : MonoBehaviour
+{
     GameObject selectedObject;
-	// Use this for initialization
-	void Start () {
-       
+    RaycastHit selectedCoordinate; //Stores the PCA coordinate 
+    private LineRenderer lr;
+    // Use this for initialization
+    void Start()
+    {
+        lr = GetComponent<LineRenderer>();
     }
-	
-	// Update is called once per frame
-	void Update () {
-        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
-        RaycastHit hitInfo;
-        if(Physics.Raycast(ray, out hitInfo))
-        {
 
-            GameObject hitObject = hitInfo.transform.root.gameObject;
-            Debug.Log("Mouse is over: " + hitInfo.collider.name);
-            if (hitInfo.collider.name != "Plane")
-                SelectObject(hitObject, hitInfo);
-                
-        }
-        else
-       {
-            ClearSelection();
-        }
-		
-	}
-
-    void SelectObject(GameObject obj, RaycastHit point)
+    // Update is called once per frame
+    void Update()
     {
 
 
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+        RaycastHit hitInfo;
+        Debug.DrawRay(ray.origin, ray.direction, Color.red, 1000, false);
+        if (Physics.Raycast(ray, out hitInfo))
+        {
+            GameObject hitObject = hitInfo.transform.root.gameObject;
+            lr.SetPosition(1, new Vector3(0, 0, hitInfo.distance));
+            if (hitInfo.collider.name != "Plane" && Input.GetMouseButton(0))
+            {
+                // Debug.Log("Mouse is over: " + hitInfo.collider.name);
+                SelectObject(hitObject, hitInfo);
+            }
+        }
+        else
+        {
+            lr.SetPosition(1, new Vector3(0, 0, 5000));
+            ClearSelection();
+        }
+    }
+
+    void SelectObject(GameObject obj, RaycastHit point)
+    {
+        Debug.Log("Objeto selecionado " + point.transform.name);
+
         if (selectedObject != null)
         {
-            if (obj == selectedObject)
+            if (point.transform.name == selectedCoordinate.transform.name)
                 return;
 
             ClearSelection();
         }
         selectedObject = obj;
+        selectedCoordinate = point;
 
-       Renderer[] rs = selectedObject.GetComponentsInChildren<Renderer>();
-      
-      
+        Renderer[] rs = selectedObject.GetComponentsInChildren<Renderer>();
+        //   GameObject r = GameObject.Find(point.transform.name);
+
+
         foreach (Renderer r in rs)
         {
+            //    Debug.Log("Obj anterior " + r.name + "Obj atual: " + point.transform.name);
+
             if (point.transform.name == r.name)
-            { 
-       
+            {
+
                 Material m = r.material;
                 m.color = Color.green;
                 r.material = m;
             }
         }
-        
+
     }
 
     void ClearSelection()
@@ -68,10 +82,15 @@ public class MouseController : MonoBehaviour {
 
         foreach (Renderer r in rs)
         {
-            Material m = r.material;
-            m.color = Color.white;
-            r.material = m;
+
+            if (selectedCoordinate.transform.name == r.name)
+            {
+                Material m = r.material;
+                m.color = Color.white;
+                r.material = m;
+            }
         }
         selectedObject = null;
+        //selectedCoordinate = null;
     }
 }
