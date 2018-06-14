f0f122ce7b4425a9a226a3a724dd203dd06f353a
carlos victor quijano <cquijanochavez@gmail.com>
2018-06-14 16:50:16 -0300

    advancing

--- Assets/Assets/Scripts/MusicObj.cs
+++ Assets/Assets/Scripts/MusicObj.cs
@@ -2,11 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class ArtistObj : MonoBehaviour {
-
-    public string nameArtist;
-    public float hotness;
-    public bool selected = false;
+public class MusicObj : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
--- Assets/Assets/Scripts/WriteArtistName.cs
+++ Assets/Assets/Scripts/WriteArtistName.cs
@@ -20,7 +20,7 @@ public class WriteArtistName : MonoBehaviour {
             artistName.GetComponent<Button>().onClick.AddListener(
                 delegate
                 {
-                    clickFunction(artistName.GetComponent<ArtistObj>());
+                    clickFunction(artistName);
                 }
             );
         } 
@@ -28,8 +28,18 @@ public class WriteArtistName : MonoBehaviour {
         
     }
 	    
-    void clickFunction(ArtistObj artist)
+    void clickFunction(GameObject artistName)
     {
-        Debug.Log(artist.nameArtist);
+        Debug.Log(artistName.GetComponent<ArtistObj>().nameArtist);
+        artistName.GetComponent<ArtistObj>().selected = !artistName.GetComponent<ArtistObj>().selected;
+        if(!artistName.GetComponent<ArtistObj>().selected)
+            artistName.GetComponent<Image>().color = ConvertColor(255, 255, 255, 65);
+        else
+            artistName.GetComponent<Image>().color = ConvertColor(255, 255, 255, 200);
+    }
+
+    public Color ConvertColor(int r, int g, int b, int a)
+    {
+        return new Color(r / 255.0f, g / 255.0f, b / 255.0f, a / 255.0f);
     }
 }
