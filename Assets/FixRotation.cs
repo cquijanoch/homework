a29f6a3bb257d16ce72da51368d004b41fd6b78c
carlos victor quijano <cquijanochavez@gmail.com>
2018-06-19 16:08:36 -0300

    all

---
+++ Assets/FixRotation.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FixRotation : MonoBehaviour {
+
+
+    void Awake()
+    {
+       
+    }
+    void Update()
+    {
+        transform.eulerAngles = new Vector3(
+            0,
+            transform.eulerAngles.y,
+            0
+        );
+    }
+}
