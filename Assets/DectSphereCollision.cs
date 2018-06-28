005b7bb9fcccbd259f385af0c34232e3e9b66042
carlos victor quijano <cquijanochavez@gmail.com>
2018-06-28 15:10:22 -0300

    janelas

---
+++ Assets/DectSphereCollision.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DectSphereCollision : MonoBehaviour {
+
+    void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.layer == 8)
+        {
+            collision.gameObject.SetActive(false);
+        }
+    }
+    void OnCollisionExit(Collision collision)
+    {
+        if (collision.gameObject.layer == 8)
+        {
+            collision.gameObject.SetActive(true);
+        }
+    }
+}
